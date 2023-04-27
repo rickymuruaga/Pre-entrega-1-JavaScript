8fde727c4867d9aee14c1373a103440b57f7ad16
Ricardo Muruaga <richardmuru25@gmail.com>
2023-04-27 18:43:27 -0300

    maquetando

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
 /* ALGORITMO CON UN CONDICIONAL */
 
-let entrada = prompt("¿Tienes ticket para entrar al estadio?");
+/* let entrada = prompt("¿Tienes ticket para entrar al estadio?");
 
 if(entrada ==="si"){
     alert("Puede Ingresar")
 }
 else{
     alert("No puede ingresar")
-}
+} */
+
+
+
+
+
+
+
 
 /* ALGORITMO UTILIZANDO UN CICLO */
 
-for(let ticket = 1; ticket <=10; ticket++){
+/*  for(let ticket = 1; ticket <=10; ticket++){
     let apellido = prompt("Ingrese su apellido");
     alert(`Entrada #${ticket} para ${apellido}`)
 }
 
-alert("No hay mas tickets")
+alert("No hay mas tickets")  */
+
+
+
+
+
+
+
+
+/* SIMULADOR INTERACTIVO */
+
+function gastos(num1 , num2, resta){
+    switch (resta) {
+     case "-":
+       return num1 - num2;
+    }
+}
+
+let num1 = parseInt(prompt("Ingrese sueldo"));
+let resta = prompt("ingrese signo -");
+let num2 = parseInt(prompt("Ingrese Gasto"));
+
+
+let resultado = gastos(num1, num2, resta);
+alert(`Te quedan ${resultado} $pesos`);

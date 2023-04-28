1f0a74ea11978047bd8ff158f0e5e61c16ba8da4
Ricardo Muruaga <richardmuru25@gmail.com>
2023-04-28 18:17:45 -0300

    probando simulador

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ alert("No hay mas tickets")  */
 
 /* SIMULADOR INTERACTIVO */
 
-let dia = prompt("NUESTROS TECNICOS DE INTERNET POR FIBRA OPTICA ESTAN DISPONIBLES DE MARTES A SÁBADO. INDIQUE EL DIA QUE DESEE.");
+/* let dia = prompt("NUESTROS TECNICOS DE INTERNET POR FIBRA OPTICA ESTAN DISPONIBLES DE MARTES A SÁBADO. INDIQUE EL DIA QUE DESEE.");
 
 let horario  = parseInt( prompt ("LOS HORARIOS SON DE 9 18 HRS. INDIQUE HORARIO QUE DESEE."));
 
@@ -52,4 +52,35 @@ function vistaTecnica(dia,horario,confirmar){
     }
 }
 
-vistiaTecnica(dia,horario,confirmar);
+vistiaTecnica(dia,horario,confirmar); */
+
+let dia = prompt("NUESTROS TÉCNICOS DE INTERNET POR FIBRA ÓPTICA ESTÁN DISPONIBLES DE MARTES A SÁBADO. INDIQUE EL DÍA QUE DESEE.");
+  let horario = parseInt(prompt("LOS HORARIOS SON DE 9 A 18 HRS. INDIQUE EL HORARIO QUE DESEE."));
+  let confirmar = prompt(`¿DESEA CONFIRMAR LA VISITA PARA EL DIA ${dia} A LAS ${horario} ? si || no`);
+
+
+  while (confirmar === "no") {
+    let dia = prompt("NUESTROS TÉCNICOS DE INTERNET POR FIBRA ÓPTICA ESTÁN DISPONIBLES DE MARTES A SÁBADO. INDIQUE EL DÍA QUE DESEE.");
+    let horario = parseInt(prompt("LOS HORARIOS SON DE 9 A 18 HRS. INDIQUE EL HORARIO QUE DESEE."));
+    confirmar = prompt(`¿DESEA CONFIRMAR LA VISITA PARA EL DIA ${dia} A LAS ${horario} ? si || no`);
+    visitaTecnica(dia, horario, confirmar);
+  }
+
+  function visitaTecnica(dia, horario, confirmar) {
+    if (confirmar === "si") {
+      if (dia === "lunes" || dia === "domingo") {
+        alert("Lo siento, no podemos programar visitas técnicas los lunes ni domingos");
+      } else if (horario < 9 || horario > 18) {
+        alert("Lo siento, nuestro horario de atención es de 9 a 18 hrs");
+      } else {
+        alert(`Nuestro técnico visitará su domicilio el día ${dia}, a las ${horario} hrs`);
+      }
+    } else if (confirmar === "no") {
+      alert("Ingrese datos nuevamente si desea una visita técnica");
+    } else {
+      alert("Operación inválida");
+    }
+  }
+  
+
+  visitaTecnica(dia, horario, confirmar);
